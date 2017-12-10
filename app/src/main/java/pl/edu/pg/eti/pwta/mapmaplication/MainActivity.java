a19f39479b4f6e3e88fb401d70a6fa19d1fd0869
Khaos <mzablot@gmail.com>
2017-12-10 19:51:14 +0100

    Metoda onClick

--- app/src/main/java/pl/edu/pg/eti/pwta/mapmaplication/MainActivity.java
+++ app/src/main/java/pl/edu/pg/eti/pwta/mapmaplication/MainActivity.java
@@ -2,14 +2,17 @@ package pl.edu.pg.eti.pwta.mapmaplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button btnSwitch;
     private TextView textRUX, textRUY, textLDX, textLDY;
+    private EditText editXup, editYup, editXdn, editYdn;
     Boolean bSwitch = false;
 
     @Override
@@ -24,6 +27,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         textLDX = findViewById(R.id.positionXdn);
         textLDY = findViewById(R.id.positionYdn);
 
+        editXup = findViewById(R.id.setXup);
+        editYup = findViewById(R.id.setYup);
+        editXdn = findViewById(R.id.setXdn);
+        editYdn = findViewById(R.id.setYdn);
+
     }
     @Override
     public void onClick(View v){
@@ -34,6 +42,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         textLDX.setText(getResources().getText(R.string.ld_positionX));
         textLDY.setText(getResources().getText(R.string.ld_positionY));
 
+        editXup.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editYup.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editXdn.setInputType(InputType.TYPE_CLASS_NUMBER);
+        editYdn.setInputType(InputType.TYPE_CLASS_NUMBER);
+
         bSwitch = false;
     }else {
         btnSwitch.setText(getResources().getText(R.string.btnSwitchFL));
@@ -41,6 +54,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         textRUY.setText(getResources().getText(R.string.ru_positionL));
         textLDX.setText(getResources().getText(R.string.ld_positionF));
         textLDY.setText(getResources().getText(R.string.ld_positionL));
+
+        editXup.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editYup.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editXdn.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        editYdn.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
         bSwitch = true;
     }
