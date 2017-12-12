7ec6dab65843db7be387fe760fb5d54fc58832c3
Jacek Polański <kontakt@jpolanski.pl>
2017-12-12 02:46:54 +0100

    Pobieranie mapy z serwera

--- app/src/main/java/pl/edu/pg/eti/pwta/mapmaplication/MainActivity.java
+++ app/src/main/java/pl/edu/pg/eti/pwta/mapmaplication/MainActivity.java
@@ -1,12 +1,28 @@
 package pl.edu.pg.eti.pwta.mapmaplication;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.ksoap2.SoapEnvelope;
+import org.ksoap2.SoapFault;
+import org.ksoap2.serialization.SoapObject;
+import org.ksoap2.serialization.SoapPrimitive;
+import org.ksoap2.serialization.SoapSerializationEnvelope;
+import org.ksoap2.transport.HttpTransportSE;
+import org.ksoap2.serialization.PropertyInfo;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
@@ -15,10 +31,17 @@ public class MainActivity extends Activity implements View.OnClickListener {
     private EditText editXup, editYup, editXdn, editYdn;
     Boolean bSwitch = false;
 
+    private static final String NAMESPACE = "map";
+    private static String URL = "http://192.168.1.33:8080/MapWS/MapWS";
+    private static final String METHOD_NAME = "getMap";
+    private static final String SOAP_ACTION = "map/getMap";
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         btnSwitch = findViewById(R.id.btnSwitch);
         btnSwitch.setOnClickListener(this);
 
@@ -32,35 +55,75 @@ public class MainActivity extends Activity implements View.OnClickListener {
         editXdn = findViewById(R.id.setXdn);
         editYdn = findViewById(R.id.setYdn);
 
+        image = findViewById(R.id.mapsView);
+
+        new RetrieveMapTask().execute();
     }
+
+    class RetrieveMapTask extends AsyncTask<String, Void, Object> {
+
+        private Exception exception;
+
+        protected Object doInBackground(String... urls) {
+            try {
+                SoapObject request = new SoapObject(NAMESPACE, METHOD_NAME);
+
+                SoapSerializationEnvelope envelope = new SoapSerializationEnvelope(SoapEnvelope.VER11);
+                envelope.setOutputSoapObject(request);
+                HttpTransportSE androidHttpTransport = new HttpTransportSE(URL);
+
+                try {
+                    androidHttpTransport.call(SOAP_ACTION, envelope);
+                } catch (IOException | XmlPullParserException e) {
+                    e.printStackTrace();
+                }
+
+                SoapPrimitive resultsRequestSOAP = (SoapPrimitive) envelope.getResponse();
+
+                return resultsRequestSOAP;
+            } catch (Exception e) {
+                this.exception = e;
+
+                return null;
+            }
+        }
+
+        protected void onPostExecute(Object map) {
+            final byte[] decodedBytes = Base64.decode(map.toString(), Base64.DEFAULT);
+            Bitmap decodedBitmap = BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+
+            image.setImageBitmap(decodedBitmap);
+        }
+    }
+
     @Override
-    public void onClick(View v){
-    if(bSwitch == true){
-        btnSwitch.setText(getResources().getText(R.string.btnSwitchXY));
-        textRUX.setText(getResources().getText(R.string.ru_positionX));
-        textRUY.setText(getResources().getText(R.string.ru_positionY));
-        textLDX.setText(getResources().getText(R.string.ld_positionX));
-        textLDY.setText(getResources().getText(R.string.ld_positionY));
+    public void onClick(View v) {
+        if (bSwitch) {
+            btnSwitch.setText(getResources().getText(R.string.btnSwitchXY));
+            textRUX.setText(getResources().getText(R.string.ru_positionX));
+            textRUY.setText(getResources().getText(R.string.ru_positionY));
+            textLDX.setText(getResources().getText(R.string.ld_positionX));
+            textLDY.setText(getResources().getText(R.string.ld_positionY));
 
-        editXup.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editYup.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editXdn.setInputType(InputType.TYPE_CLASS_NUMBER);
-        editYdn.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editXup.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editYup.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editXdn.setInputType(InputType.TYPE_CLASS_NUMBER);
+            editYdn.setInputType(InputType.TYPE_CLASS_NUMBER);
 
-        bSwitch = false;
-    }else {
-        btnSwitch.setText(getResources().getText(R.string.btnSwitchFL));
-        textRUX.setText(getResources().getText(R.string.ru_positionF));
-        textRUY.setText(getResources().getText(R.string.ru_positionL));
-        textLDX.setText(getResources().getText(R.string.ld_positionF));
-        textLDY.setText(getResources().getText(R.string.ld_positionL));
+            bSwitch = false;
+        } else {
+            btnSwitch.setText(getResources().getText(R.string.btnSwitchFL));
+            textRUX.setText(getResources().getText(R.string.ru_positionF));
+            textRUY.setText(getResources().getText(R.string.ru_positionL));
+            textLDX.setText(getResources().getText(R.string.ld_positionF));
+            textLDY.setText(getResources().getText(R.string.ld_positionL));
 
-        editXup.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        editYup.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        editXdn.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
-        editYdn.setInputType(InputType.TYPE_CLASS_NUMBER |InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            editXup.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            editYup.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            editXdn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            editYdn.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
 
-        bSwitch = true;
+            bSwitch = true;
+        }
     }
-}
 }
